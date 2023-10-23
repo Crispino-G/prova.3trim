26f77549e88296fd06b8f880544df4237f53bd4f
Crispino-G <130381129+Crispino-G@users.noreply.github.com>
2023-10-23 08:43:04 -0300

    Add files via upload

---
+++ prova_crispino/frontEnd3trimLeoncio-main/index.js
@@ -0,0 +1,17 @@
+<function> 
+ 
+    
+    Calcular(){
+
+    let .idadeCanina = document.querySelector("Qual a idade do cachorrinho?");
+    mostraResultado.textContent = ("Resultado: a idade humana do cachorrinho é 1 + 7 idadeCanina")
+    .querySelector = Resultado
+    }
+  
+       
+    
+    
+    <link rel = "index.css"> </link>
+    <script snc="index.html"></script>
+      
+</function>
